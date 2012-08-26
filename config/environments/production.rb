e50ee67517de1ed79eb64db4e345dcbf5181773b
Sunil Prakash <prakashsunil@gmail.com>
2012-08-26 18:52:48 +0530

    tyring a prod fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,11 @@ Commute::Application.configure do
   config.assets.compress = true
 
   config.assets.precompile += %w[ *.css *.js ]
+  config.assets.initialize_on_precompile = false
 
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
