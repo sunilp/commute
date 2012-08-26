2936f0d06c50be55599bbc1681c40ecab5746429
Sunil Prakash <prakashsunil@gmail.com>
2012-08-26 18:15:04 +0530

    fixed help issue

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Commute::Application.routes.draw do
   match '/home', :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
+  match 'help', to: 'pages#help'
 
 
 
