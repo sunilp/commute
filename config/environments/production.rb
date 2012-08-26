154d6911e60dbbb57ad5bbb45f2d02001f117b54
Sunil Prakash <prakashsunil@gmail.com>
2012-08-26 18:28:18 +0530

    prod fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Commute::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.precompile += %w[ bootstrap/*.css bootstrap/*.js ]
+
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
