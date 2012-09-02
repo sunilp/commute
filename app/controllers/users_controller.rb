f3f522fdd4b57a7648d75a24c1463cb049c78453
Sunil Prakash <prakashsunil@gmail.com>
2012-09-02 19:29:10 +0530

    added email support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,7 @@ class UsersController < ApplicationController
 def create
     @user = User.new(params[:user])
     if @user.save
+      Notifier.signup_email(@user).deliver
       sign_in @user
       flash[:success] = "Welcome to Commute!"	
       redirect_to @user
